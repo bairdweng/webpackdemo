af5445667b54190aba812bd086614340c2f4a970
Baird-weng <644672334@qq.com>
2019-05-17 19:03:15 +0800

    dwd

---
+++ src/app/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <div>自定义的脚手架哦</div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'hello vue !!',
+    }
+  },
+}
+</script>
+<style >
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,74 @@
 
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
   // 入口文件。
-  mode: 'development',
-  entry: __dirname + "/app/main.js",
+  mode: 'none',
+  entry: './src/app/main.ts',
   // 出口文件
   output: {
-    path: __dirname + "/public",
-    filename: "bundle.js"
-   },
-   // source-map 便于调试。
-   devtool: 'eval-source-map', 
-   // 本地服务器
-   devServer: {
-    contentBase: __dirname + "/public",
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'main.js',
+  },
+  // source-map 便于调试。
+  devtool: 'eval-source-map',
+  // 本地服务器
+  devServer: {
+    contentBase: `${__dirname}`,
     compress: false,
-    host:"127.0.0.1", 
-    port: 8900
-  }
-
+    host: '127.0.0.1',
+    port: 8200,
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: file => (
+          /node_modules/.test(file)
+          && !/\.vue\.js/.test(file)
+        ),
+      },
+      {
+        test: /\.css$/,
+        // css 加载器
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      // url-loader 功能类似于 file-loader，但是在文件大小（单位 byte）低于指定的限制时，可以返回一个 DataURL。
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+      // 对模版文件使用loader
+        test: /\.ejs$/,
+        use: 'ejs-loader',
+      },
+    ],
+  },
 }
